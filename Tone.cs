e32af4f7b590ae6afb8d6a7269f9dfb08481a735
parasixx <parasixx@gmail.com>
2014-09-25 08:54:49 +0200

    created first methods of tone class

--- Tone.cs
+++ Tone.cs
@@ -2,11 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Midi;
 
 namespace bubblegum_sequencer
 {
     class Tone
     {
+        private Note note;
 
+        public Tone(Note aNote)
+        {
+            note = aNote;
+        }
+
+        public void play()
+        {
+            
+        }
     }
 }
