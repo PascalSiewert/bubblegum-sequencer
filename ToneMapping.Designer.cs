0246430ca3b13505bc501cb33a5fdc35c7411eb0
FrankderVernichter <frust.furz@web.de>
2014-11-02 16:53:04 +0100

    ToneMappingGUI
    Oberfläche für die Zuordnung der Töne zu den Farben.

---
+++ ToneMapping.Designer.cs
@@ -0,0 +1,370 @@
+﻿namespace bubblegum_sequencer
+{
+    partial class ToneMapping
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TonePlay1btn = new System.Windows.Forms.Button();
+            this.Farbe1cb = new System.Windows.Forms.ComboBox();
+            this.Ton1cb = new System.Windows.Forms.ComboBox();
+            this.Ton2cb = new System.Windows.Forms.ComboBox();
+            this.Farbe2cb = new System.Windows.Forms.ComboBox();
+            this.Ton3cb = new System.Windows.Forms.ComboBox();
+            this.Farbe3cb = new System.Windows.Forms.ComboBox();
+            this.Ton4cb = new System.Windows.Forms.ComboBox();
+            this.Farbe4cb = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.Abbrechenbtn = new System.Windows.Forms.Button();
+            this.Bestaetigenbtn = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.Ton5cb = new System.Windows.Forms.ComboBox();
+            this.Farbe5cb = new System.Windows.Forms.ComboBox();
+            this.TonePlay2btn = new System.Windows.Forms.Button();
+            this.TonePlay3btn = new System.Windows.Forms.Button();
+            this.TonePlay4btn = new System.Windows.Forms.Button();
+            this.TonePlay5btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Töne Auswählen";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Farbe:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(195, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Ton:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(377, 60);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Abspielen";
+            // 
+            // TonePlay1btn
+            // 
+            this.TonePlay1btn.Location = new System.Drawing.Point(380, 78);
+            this.TonePlay1btn.Name = "TonePlay1btn";
+            this.TonePlay1btn.Size = new System.Drawing.Size(75, 23);
+            this.TonePlay1btn.TabIndex = 5;
+            this.TonePlay1btn.Text = "Play";
+            this.TonePlay1btn.UseVisualStyleBackColor = true;
+            this.TonePlay1btn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Farbe1cb
+            // 
+            this.Farbe1cb.FormattingEnabled = true;
+            this.Farbe1cb.Location = new System.Drawing.Point(37, 76);
+            this.Farbe1cb.Name = "Farbe1cb";
+            this.Farbe1cb.Size = new System.Drawing.Size(121, 21);
+            this.Farbe1cb.TabIndex = 6;
+            this.Farbe1cb.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Ton1cb
+            // 
+            this.Ton1cb.FormattingEnabled = true;
+            this.Ton1cb.Location = new System.Drawing.Point(198, 78);
+            this.Ton1cb.Name = "Ton1cb";
+            this.Ton1cb.Size = new System.Drawing.Size(121, 21);
+            this.Ton1cb.TabIndex = 7;
+            // 
+            // Ton2cb
+            // 
+            this.Ton2cb.FormattingEnabled = true;
+            this.Ton2cb.Location = new System.Drawing.Point(198, 109);
+            this.Ton2cb.Name = "Ton2cb";
+            this.Ton2cb.Size = new System.Drawing.Size(121, 21);
+            this.Ton2cb.TabIndex = 9;
+            // 
+            // Farbe2cb
+            // 
+            this.Farbe2cb.FormattingEnabled = true;
+            this.Farbe2cb.Location = new System.Drawing.Point(37, 109);
+            this.Farbe2cb.Name = "Farbe2cb";
+            this.Farbe2cb.Size = new System.Drawing.Size(121, 21);
+            this.Farbe2cb.TabIndex = 8;
+            // 
+            // Ton3cb
+            // 
+            this.Ton3cb.FormattingEnabled = true;
+            this.Ton3cb.Location = new System.Drawing.Point(198, 138);
+            this.Ton3cb.Name = "Ton3cb";
+            this.Ton3cb.Size = new System.Drawing.Size(121, 21);
+            this.Ton3cb.TabIndex = 11;
+            // 
+            // Farbe3cb
+            // 
+            this.Farbe3cb.FormattingEnabled = true;
+            this.Farbe3cb.Location = new System.Drawing.Point(37, 138);
+            this.Farbe3cb.Name = "Farbe3cb";
+            this.Farbe3cb.Size = new System.Drawing.Size(121, 21);
+            this.Farbe3cb.TabIndex = 10;
+            // 
+            // Ton4cb
+            // 
+            this.Ton4cb.FormattingEnabled = true;
+            this.Ton4cb.Location = new System.Drawing.Point(198, 169);
+            this.Ton4cb.Name = "Ton4cb";
+            this.Ton4cb.Size = new System.Drawing.Size(121, 21);
+            this.Ton4cb.TabIndex = 13;
+            // 
+            // Farbe4cb
+            // 
+            this.Farbe4cb.FormattingEnabled = true;
+            this.Farbe4cb.Location = new System.Drawing.Point(37, 167);
+            this.Farbe4cb.Name = "Farbe4cb";
+            this.Farbe4cb.Size = new System.Drawing.Size(121, 21);
+            this.Farbe4cb.TabIndex = 12;
+            this.Farbe4cb.SelectedIndexChanged += new System.EventHandler(this.comboBox8_SelectedIndexChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 81);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(16, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "1.";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 112);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(16, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "2.";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 141);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(16, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "3.";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 170);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(16, 13);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "4.";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(7, 60);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(21, 13);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Nr:";
+            // 
+            // Abbrechenbtn
+            // 
+            this.Abbrechenbtn.Location = new System.Drawing.Point(31, 265);
+            this.Abbrechenbtn.Name = "Abbrechenbtn";
+            this.Abbrechenbtn.Size = new System.Drawing.Size(75, 23);
+            this.Abbrechenbtn.TabIndex = 19;
+            this.Abbrechenbtn.Text = "Abbrechen";
+            this.Abbrechenbtn.UseVisualStyleBackColor = true;
+            // 
+            // Bestaetigenbtn
+            // 
+            this.Bestaetigenbtn.Location = new System.Drawing.Point(112, 265);
+            this.Bestaetigenbtn.Name = "Bestaetigenbtn";
+            this.Bestaetigenbtn.Size = new System.Drawing.Size(75, 23);
+            this.Bestaetigenbtn.TabIndex = 20;
+            this.Bestaetigenbtn.Text = "Bestätigen";
+            this.Bestaetigenbtn.UseVisualStyleBackColor = true;
+            this.Bestaetigenbtn.Click += new System.EventHandler(this.Bestaetigenbtn_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(12, 199);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(16, 13);
+            this.label10.TabIndex = 23;
+            this.label10.Text = "5.";
+            // 
+            // Ton5cb
+            // 
+            this.Ton5cb.FormattingEnabled = true;
+            this.Ton5cb.Location = new System.Drawing.Point(198, 198);
+            this.Ton5cb.Name = "Ton5cb";
+            this.Ton5cb.Size = new System.Drawing.Size(121, 21);
+            this.Ton5cb.TabIndex = 22;
+            this.Ton5cb.SelectedIndexChanged += new System.EventHandler(this.comboBox9_SelectedIndexChanged);
+            // 
+            // Farbe5cb
+            // 
+            this.Farbe5cb.FormattingEnabled = true;
+            this.Farbe5cb.Location = new System.Drawing.Point(37, 196);
+            this.Farbe5cb.Name = "Farbe5cb";
+            this.Farbe5cb.Size = new System.Drawing.Size(121, 21);
+            this.Farbe5cb.TabIndex = 21;
+            // 
+            // TonePlay2btn
+            // 
+            this.TonePlay2btn.Location = new System.Drawing.Point(380, 107);
+            this.TonePlay2btn.Name = "TonePlay2btn";
+            this.TonePlay2btn.Size = new System.Drawing.Size(75, 23);
+            this.TonePlay2btn.TabIndex = 24;
+            this.TonePlay2btn.Text = "Play";
+            this.TonePlay2btn.UseVisualStyleBackColor = true;
+            this.TonePlay2btn.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // TonePlay3btn
+            // 
+            this.TonePlay3btn.Location = new System.Drawing.Point(380, 136);
+            this.TonePlay3btn.Name = "TonePlay3btn";
+            this.TonePlay3btn.Size = new System.Drawing.Size(75, 23);
+            this.TonePlay3btn.TabIndex = 25;
+            this.TonePlay3btn.Text = "Play";
+            this.TonePlay3btn.UseVisualStyleBackColor = true;
+            // 
+            // TonePlay4btn
+            // 
+            this.TonePlay4btn.Location = new System.Drawing.Point(380, 165);
+            this.TonePlay4btn.Name = "TonePlay4btn";
+            this.TonePlay4btn.Size = new System.Drawing.Size(75, 23);
+            this.TonePlay4btn.TabIndex = 26;
+            this.TonePlay4btn.Text = "Play";
+            this.TonePlay4btn.UseVisualStyleBackColor = true;
+            // 
+            // TonePlay5btn
+            // 
+            this.TonePlay5btn.Location = new System.Drawing.Point(380, 194);
+            this.TonePlay5btn.Name = "TonePlay5btn";
+            this.TonePlay5btn.Size = new System.Drawing.Size(75, 23);
+            this.TonePlay5btn.TabIndex = 27;
+            this.TonePlay5btn.Text = "Play";
+            this.TonePlay5btn.UseVisualStyleBackColor = true;
+            // 
+            // ToneMapping
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(479, 309);
+            this.Controls.Add(this.TonePlay5btn);
+            this.Controls.Add(this.TonePlay4btn);
+            this.Controls.Add(this.TonePlay3btn);
+            this.Controls.Add(this.TonePlay2btn);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.Ton5cb);
+            this.Controls.Add(this.Farbe5cb);
+            this.Controls.Add(this.Bestaetigenbtn);
+            this.Controls.Add(this.Abbrechenbtn);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Ton4cb);
+            this.Controls.Add(this.Farbe4cb);
+            this.Controls.Add(this.Ton3cb);
+            this.Controls.Add(this.Farbe3cb);
+            this.Controls.Add(this.Ton2cb);
+            this.Controls.Add(this.Farbe2cb);
+            this.Controls.Add(this.Ton1cb);
+            this.Controls.Add(this.Farbe1cb);
+            this.Controls.Add(this.TonePlay1btn);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "ToneMapping";
+            this.Text = "ToneMapping";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button TonePlay1btn;
+        private System.Windows.Forms.ComboBox Farbe1cb;
+        private System.Windows.Forms.ComboBox Ton1cb;
+        private System.Windows.Forms.ComboBox Ton2cb;
+        private System.Windows.Forms.ComboBox Farbe2cb;
+        private System.Windows.Forms.ComboBox Ton3cb;
+        private System.Windows.Forms.ComboBox Farbe3cb;
+        private System.Windows.Forms.ComboBox Ton4cb;
+        private System.Windows.Forms.ComboBox Farbe4cb;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button Abbrechenbtn;
+        private System.Windows.Forms.Button Bestaetigenbtn;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.ComboBox Ton5cb;
+        private System.Windows.Forms.ComboBox Farbe5cb;
+        private System.Windows.Forms.Button TonePlay2btn;
+        private System.Windows.Forms.Button TonePlay3btn;
+        private System.Windows.Forms.Button TonePlay4btn;
+        private System.Windows.Forms.Button TonePlay5btn;
+    }
+}
---
+++ ToneMapping.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace bubblegum_sequencer
+{
+    public partial class ToneMapping : Form
+    {
+        public ToneMapping()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Bestaetigenbtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox8_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox9_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
